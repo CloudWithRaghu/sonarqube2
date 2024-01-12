e39e085f7bff9586a54c11c6eee6e74e087f958d
Raghu <154458583+CloudWithRaghu@users.noreply.github.com>
2024-01-12 12:19:32 +0530

    Update DateServlet.java

--- src/main/java/com/acme/DateServlet.java
+++ src/main/java/com/acme/DateServlet.java
@@ -30,7 +30,11 @@ public class DateServlet extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        req.getRequestDispatcher("/test/tag2.jsp").forward(req,resp);
-    }
+    try {
+    InetAddress addr = InetAddress.getByName(request.getRemoteAddr());
+    //...
+  }
+  catch (UnknownHostException ex) {  // Compliant
+    //...
+  }
 }
